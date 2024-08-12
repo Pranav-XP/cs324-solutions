88721a37e255ccfaac6736f9eecf4af531aaa392
Pranav <112617967+Pranav-XP@users.noreply.github.com>
2024-08-13 10:17:56 +1200

    added main class to run program

--- src/main/java/dev/pranavtech/week4/Main.java
+++ src/main/java/dev/pranavtech/week4/Main.java
@@ -1,5 +1,19 @@
 package dev.pranavtech.week4;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Number n = new Number();
+        Scanner input = new Scanner(System.in);
+
+        new HexNumber(n);
+        new BinNumber(n);
+
+        while(true){
+            System.out.println("\nEnter a number: ");
+            n.setValue(input.nextInt());
+        }
+    }
 
 }
